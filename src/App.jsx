104959167e5d6a0c5386bbb7f89ba4cf4d20bd93
Tonalli López <aguilablanca_7@live.com>
2021-08-12 10:47:54 -0500

    REFACTOR: Move one level down useAppContext

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,15 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import { useAppContext } from "@context";
 
 import Layout from "@components/Layout";
 import Home from "@pages/Home";
 
 export default function App() {
-  const [{ boot }] = useAppContext();
-
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" render={(props) => <Home boot={boot} {...props} />} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </Layout>
     </BrowserRouter>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { useAppContext } from "@context";
 
 import "@stylesPages/Home.scss";
 
 export default function Home() {
+  const [{ boot: { home: boot } }] = useAppContext();
+
   return (
     <div className="home">
-      <h1 className="home__title">Home</h1>
+      <h1 className="home__title">{boot}</h1>
     </div>
   );
 }
