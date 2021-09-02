fb3cf4c31b5edb3c852f52302dbbb43fc43ce978
Tonalli López <aguilablanca_7@live.com>
2021-09-02 15:08:51 -0500

    REFACTOR: Add React.lazy to pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
-import React from "react";
+import React, { lazy, Suspense } from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 
 import Layout from "@components/Layout";
-import Home from "@pages/Home";
+import { LoaderPage } from "@components/Loaders";
+
+const Home = lazy(() => import("@pages/Home"));
 
 export default function App() {
   return (
     <BrowserRouter>
       <Layout>
         <Switch>
-          <Route exact path="/" component={Home} />
+          <Suspense fallback={<LoaderPage />}>
+            <Route exact path="/" component={Home} />
+          </Suspense>
         </Switch>
       </Layout>
     </BrowserRouter>
