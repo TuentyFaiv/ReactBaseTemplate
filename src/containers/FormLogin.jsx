da64bb296b805174312f92f82c604a173a2c75fd
Tonalli López <aguilablanca_7@live.com>
2021-09-02 10:18:55 -0500

    FEAT: Add container for login form

---
+++ src/containers/FormLogin.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import { Formik, Form } from "formik";
+import { LoginSchema } from "@schemas";
+
+import Input from "@components/Input";
+
+export default function FormLogin({ boot: { login: boot, formik: bootFormik } }) {
+
+  const handleSubmit = async (values) => {
+    try {
+      console.log(values);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <Formik
+      initialValues={{
+        email: "",
+        password: ""
+      }}
+      validationSchema={LoginSchema(bootFormik)}
+      onSubmit={(values) => {
+        handleSubmit(values);
+      }}
+    >
+      {({ isSubmitting, errors }) => (
+        <Form>
+          <Input
+            label={boot.email}
+            name="email"
+            type="text"
+            placeholder={boot.email}
+          />
+          <Input
+            label={boot.passowrd}
+            name="password"
+            type="password"
+            placeholder={boot.passowrd}
+          />
+          <button
+            type="submit"
+            disabled={(isSubmitting || Object.keys(errors).length > 0)}
+          >
+            {boot.submit}
+          </button>
+        </Form>
+      )}
+    </Formik>
+  );
+}
