dfa414e28bfc1ba1116058c72395c36c69d35a8c
Tonalli López <aguilablanca_7@live.com>
2022-01-31 18:43:18 -0600

    🔧 REFACTOR: Add example without lazy and refactor mixins

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import { StrictMode } from "react";
 import { render } from "react-dom";
 import { I18nextProvider } from "react-i18next";
 import { AppContextProvider } from "@context";
@@ -8,10 +9,13 @@ import App from "./App";
 import "./styles/General.scss";
 
 render(
-  <AppContextProvider>
-    <I18nextProvider i18n={i18n}>
-      <App />
-    </I18nextProvider>
-  </AppContextProvider>,
+  <StrictMode>
+
+    <AppContextProvider>
+      <I18nextProvider i18n={i18n}>
+        <App />
+      </I18nextProvider>
+    </AppContextProvider>
+  </StrictMode>,
   document.getElementById("root")
 );
