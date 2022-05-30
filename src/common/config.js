5b51584145b6778bc6e054fe4140b8e757641b86
Tonalli López <aguilablanca_7@live.com>
2022-05-30 13:04:13 -0500

    🏗️ REFACTOR: Move to TypeScript

--- src/common/config.js
+++
@@ -1,6 +0,0 @@
-export default {
-  api: process.env.API || "http://localhost:5000",
-  api_local: process.env.API_LOCAL || "http://localhost:3000",
-  ipregistry_key: process.env.IPREGISTRY_KEY,
-  ipregistry_url: process.env.IPREGISTRY_URL
-};
--- src/hooks/index.js
+++
@@ -1,4 +0,0 @@
-export { default as useModal } from "./useModal";
-export { default as useGetCountry } from "./useGetCountry";
-export { default as useQuery } from "./useQuery";
-export { default as useDatas } from "./useDatas";
